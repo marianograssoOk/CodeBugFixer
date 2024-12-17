a52073c05a7498d178c0a3b5cc948b6f166b3c39
Mariano Grasso <44099102+marianograssoOk@users.noreply.github.com>
2024-12-17 16:37:44 -0300

    add explanation and fixed prompts

--- app.py
+++ app.py
@@ -7,6 +7,35 @@ client = OpenAI(api_key = config.API_KEY)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        code = request.form["code"]
+        error = request.form["error"]
+        prompt = (f"Explain the error in this code without fixing it: "
+                  f"\n\n{code}\n\nError:\n\n{error}")
+        model_engine = "gpt-3.5-turbo"
+        
+        explanation_completions = client.chat.completions.create(
+            model=model_engine,
+            messages=[{"role": "user", "content": f"{prompt}"}],
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.2
+        )
+        explanation = explanation_completions.choices[0].message.content
+        fixed_code_prompt = (f"Fix this code: \n\n{code}\n\nError:\n\n{error}."
+                             f" \n Respond only with the fixed code.")
+        fixed_code_completions = client.chat.completions.create(
+            model=model_engine,
+            messages=[{"role": "user", "content": f"{fixed_code_prompt}"}],
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.2
+        )
+        fixed_code = fixed_code_completions.choices[0].message.content
+         
     return render_template("index.html")
+
 if __name__ == "__main__":
     app.run()
